1a44216846559f702f755b6288642dd2eb1b78f0
tatyanarm <125110132+tatyanarm@users.noreply.github.com>
2023-07-15 13:39:33 +0200

    Update Connection_test.py

--- module_5/Connection_test.py
+++ module_5/Connection_test.py
@@ -1,7 +1,12 @@
+ #Import MongoClient
 from pymongo import MongoClient
+#Creating variable Url and assign string from Atlas whit username and password 
 client = MongoClient("mongodb+srv://admin:admin@cluster0.mj8httk.mongodb.net/?retryWrites=true&w=majority")
+#Creating Variable and assign Database Instance
 db = client.pytech
+#Print Statemant if Datatabase connected
 try: db.command("serverStatus")
 except Exception as e: print(e)
 else: print("You are connected!")
+#close client
 client.close()
