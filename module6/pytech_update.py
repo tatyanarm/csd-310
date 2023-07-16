5b549a80d5293dbfbc5940ff2c28edc89c8dfe18
tatyanarm <125110132+tatyanarm@users.noreply.github.com>
2023-07-16 03:16:37 +0200

    Update pytech_update.py

--- module6/pytech_update.py
+++ module6/pytech_update.py
@@ -1,1 +1,18 @@
+    #Import MongoClient
+from pymongo import MongoClient
+    #Creating variable Url and assign string from Atlas whit username and password 
+url = ("mongodb+srv://admin:admin@cluster0.mj8httk.mongodb.net/?retryWrites=true&w=majority")
+    #Variable whit client and calling MongoClient passing in bariable Url 
+client = MongoClient(url)
+    #Creating Variable and assign Database Instance 
+db = client.pytech
+    #Print Statemant if Datatabase Variable
+mycollection =db["students"]
 
+myquery = {"Last_Name" : "Trahald" }
+newvalues = {"$set": {"Last_Name": "Golum"}}
+mycollection.update_one(myquery, newvalues)   
+
+#print "customers" after the update:
+for x in mycollection.find({'Student_ID': 1007}):
+  print(x) 
